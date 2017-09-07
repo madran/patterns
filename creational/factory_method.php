c07b0a33df0d2909484fec4821c3c542c2dbf8cd
Łukasz Różański <lukasz.rozanski@gmail.com>
2017-09-07 20:48:31 +0200

    factory method v0.1

--- creational/factory_method.php
+++ creational/factory_method.php
@@ -1,85 +1,56 @@
 <?php
 
-class Pizza
+abstract class DocumentTemplate
 {
-    private $dough = "";
-    private $sauce = "";
-    private $topping = "";
-
-    public function setDough($dough)
+    protected $data;
+    
+    public function setData($data)
     {
-        $this->dough = $dough;
+        $this->data = $data;
     }
-
-    public function setSauce($sauce)
-    {
-        $this->sauce = $sauce;
-    }
-
-    public function setTopping($topping)
-    {
-        $this->topping = $topping;
-    }
-
-    public function __toString()
-    {
-        return "Dough: $this->dough | Suace: $this->sauce | Topping: $this->topping";
-    }
+    
+    abstract public function generateTemplate();
 }
 
-abstract class PizzaBuilder
+class ProposalDocumentTemplate extends DocumentTemplate
 {
-    protected $pizza;
-
-    public function __construct()
+    public function generateTemplate()
     {
-        $this->pizza = new Pizza();
-    }
-
-    abstract public function getPizza();
-
-    public function buildDough($dough)
-    {
-        $this->pizza->setDough($dough);
-    }
-
-    public function buildSauce($suace)
-    {
-        $this->pizza->setSauce($suace);
-    }
-
-    public function buildTopping($topping)
-    {
-        $this->pizza->setTopping($topping);
+        
     }
 }
 
-class HawaiianPizzaBuilder extends PizzaBuilder
+class StatementDocumentTemplate extends DocumentTemplate
 {
-    public function getPizza()
+    public function generateTemplate()
     {
-        $this->buildDough("cross");
-        $this->buildSauce("mild");
-        $this->buildTopping("ham+pineapple");
-
-        return $this->pizza;
+        
     }
 }
 
-class SpicyPizzaBuilder extends PizzaBuilder {
-    public function getPizza()
+abstract class Document
+{
+    public function generateDocument()
     {
-        $this->buildDough("pan baked");
-        $this->buildSauce("hot");
-        $this->buildTopping("pepperoni+salami");
+        $documentTemplate = $this->getDocumentTemplate();
+        $documentTemplate->setData($this->data);
+        
+        return $documentTemplate;
+    }
+    
+    abstract protected function getDocumentTemplate();
+}
 
-        return $this->pizza;
+class ProposalDocument extends Document
+{
+    protected function getDocumentTemplate() {
+        return new ProposalDocumentTemplate();
     }
 }
 
-
-$hawaiian_pizzabuilder = new HawaiianPizzaBuilder();
-$spicy_pizzabuilder = new SpicyPizzaBuilder();
-
-echo $hawaiian_pizzabuilder->getPizza() . PHP_EOL;
-echo $spicy_pizzabuilder->getPizza() . PHP_EOL;
+class StatementDocument extends Document
+{
+    protected function getDocumentTemplate() {
+        return new StatementDocumentTemplate();
+    }
+}
